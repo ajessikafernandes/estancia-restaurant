29c487a93a77e9b6d2b6ee602c5ec4074dbb0c0b
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2021-01-07 09:51:50 -0300

    criação da page home

---
+++ src/app/views/asking/asking.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asking',
+  templateUrl: './asking.component.html',
+  styleUrls: ['./asking.component.css']
+})
+export class AskingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  panelOpenState = false;
+
+}
---
+++ src/app/views/depositions/depositions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-depositions',
+  templateUrl: './depositions.component.html',
+  styleUrls: ['./depositions.component.css']
+})
+export class DepositionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
