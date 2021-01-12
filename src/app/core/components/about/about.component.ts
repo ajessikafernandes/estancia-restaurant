35f4d5848b3fc5ddace5833052dc5b41e7aa20be
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2021-01-12 10:16:34 -0300

    ajustando css responsivo componente about

--- src/app/core/components/about/about.component.ts
+++ src/app/core/components/about/about.component.ts
@@ -7,13 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  linkImagemSobre: any;
   nomeMarca: string;
   slogan: string;
   descricao: string;
 
   constructor() {
-    this.linkImagemSobre = 'banner-meat.jpg';
     this.nomeMarca = 'Estancia Churrascaria e Pizzaria';
     this.slogan = 'SABOR QUE VEM DOS DETALHES';
     this.descricao = 'A Estância Churrascaria e Pizzaria, oferecendo a seus clientes uma harmonia de aromas e sabores especiais. Uma variedade enorme de pratos: carnes, frango, pizzas , porções, frutos do mar, e sobremesas, feitos com o máximo controle de qualidade, pensando sempre na satisfação de nossos clientes. Nossa estrutura oferece ambiente climatizado e um salão de eventos capaz de atender cerca de 75 pessoas para buffets, aniversários, casamentos, festas de 15 anos e confraternizações em geral, também possui estacionamento com manobrista.';    ;
