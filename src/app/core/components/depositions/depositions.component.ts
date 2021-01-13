c8074716cae6515aff70a633755072d4e6cab612
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2021-01-13 16:58:13 -0300

    ajustando componente depositions

--- src/app/core/components/depositions/depositions.component.ts
+++ src/app/core/components/depositions/depositions.component.ts
@@ -9,26 +9,8 @@ export class DepositionsComponent implements OnInit {
 
   titleBanner: string;
 
-  depositions1: string;
-
-  author1: string;
-
-  depositions2: string;
-
-  author2: string;
-
-  depositions3: string;
-
-  author3: string;
-
   constructor() {
-    this.titleBanner = 'Depoimento dos nossos Clientes';
-    this.depositions1 = 'almoçei com minha namorada exelente atendimento funcionaros otimos buffe maravilhoso ambiente muito bom..adorei';
-    this.author1 = 'Daniel P.';
-    this.depositions2 = 'Fui com minha família, pedimos rodízio, fomos muito bem atendidos. Churrasco muito gostoso, várias opções. Tem estacionamento também.';;
-    this.author2 = 'Ludia';
-    this.depositions3 ='Como todo brasileiro sou apaixonado por churrasco e pizza... então nada melhor que aproveitar o rodizio nesse estabelecimento.';
-    this.author3 = 'Pedro S.';
+    this.titleBanner = 'Avaliações dos nossos Clientes';
   }
 
   ngOnInit() {
