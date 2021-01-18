06d6976929d36ea3a851fc043e492b5068448112
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2021-01-17 22:00:47 -0300

    adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './core/components/home/home.component';
 import { MenuComponent } from './core/components/menu/menu.component';
+import { PhotoComponent } from './core/components/photo/photo.component';
+import { ContactComponent } from './core/components/contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'menu',
     component: MenuComponent,
-  }
+  },
+  {
+    path: 'photo',
+    component: PhotoComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
 ];
 
 @NgModule({
