a0f2d366dc56d45d186443cde7170f9aca93104a
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2021-01-18 17:03:26 -0300

    add chamada para servico pizza

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SteakhouseService } from 'src/app/data/service/steakhouse.service';
+import { PizzeriaService } from 'src/app/data/service/pizzeria.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,14 +8,23 @@ import { SteakhouseService } from 'src/app/data/service/steakhouse.service';
   styleUrls: ['./menu.component.css'],
   providers: [
     SteakhouseService,
+    PizzeriaService,
   ]
 })
 export class MenuComponent implements OnInit {
 
   panelOpenState = false;
 
+  pageTitle: string;
+
+  titleSteak: string;
+
+  descriptionSteak: string;
+
   titleCuts1: string;
+
   titleCuts2: string;
+
   titleCuts3: string;
 
   tradicionalCutsList: any;
@@ -23,16 +33,54 @@ export class MenuComponent implements OnInit {
 
   cuttingFishList: any;
 
-  constructor(private tradicionalService: SteakhouseService) {
+  titleBannerPizza: string;
+
+  titlePizza1: string;
+
+  descriptionBannerPizza: string;
+
+  titlePizza2: string;
+
+  tradicionalPizzasList: any;
+
+  sweetPizzasList: any;
+
+  constructor(private steakhouseService: SteakhouseService, private pizzeriaService: PizzeriaService) {
+
+    this.pageTitle = 'CARDÁPIO';
+
+    this.titleSteak = 'Steak';
+
+    this.descriptionSteak = 'Carnes assadas em grelha australiana, sempre no seu ponto preferido. Cada corte passa pelo nosso mix de 17 temperos secretos antes de ser preparado com técnicas especiais, na chapa ou em chama aberta. Todos os nossos steaks são servidos com um acompanhamento à sua escolha. Se quiser, você também pode acrescentar ao seu pedido uma das nossas entradas (salada ou sopa do dia) com 50% de desconto: R$ 12,45. A gramatura dos produtos equivale ao peso in natura.';
+
     this.titleCuts1 = 'Cortes Tradicionais';
+
     this.titleCuts2 = 'Cortes Nobres';
+
     this.titleCuts3 = 'Peixes';
+
+    this.titleBannerPizza = 'Pizzas';
+
+    this.titlePizza1 = 'Pizzas Salgadas';
+
+    this.titlePizza2 = 'Pizzas Doces'
+
+    this.descriptionBannerPizza = 'Mesmo prezando pela tradição, o restaurante apresenta opções criativas em seu cardápio. Nele o cliente pode optar por sabores tradicionais, especiais e doces';
+
   }
 
   ngOnInit() {
-    this.tradicionalCutsList = this.tradicionalService.getTradicionalCuts();
-    this.novelCutsList = this.tradicionalService.getNovelCuts();
-    this.cuttingFishList = this.tradicionalService.getFishCuts();
+
+    this.tradicionalCutsList = this.steakhouseService.getTradicionalCuts();
+
+    this.novelCutsList = this.steakhouseService.getNovelCuts();
+
+    this.cuttingFishList = this.steakhouseService.getFishCuts();
+
+    this.tradicionalPizzasList = this.pizzeriaService.getTradiconalPizzas();
+
+    this.sweetPizzasList = this.pizzeriaService.getSweetPizzas();
+
   }
 
 }
